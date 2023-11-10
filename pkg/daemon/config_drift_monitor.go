6bebe6c38f61737d23525c863677cd9d70040217
Ines Qian <iqian@redhat.com>
2023-11-10 15:45:43 -0500

    machine-state-controller.openshift-machine-config-operator.svc.cluster.local

--- pkg/daemon/config_drift_monitor.go
+++ pkg/daemon/config_drift_monitor.go
@@ -264,7 +264,7 @@ func (c *configDriftWatcher) start() {
 		fmt.Printf("Failed to marshal JSON: %v\n", err)
 	}
 
-	url := "http://machine-state-controller:9090/receive"
+	url := "http://machine-state-controller.openshift-machine-config-operator.svc.cluster.local:9090/receive"
 
 	_, err = http.Post(url, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
